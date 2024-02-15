4f96169d9fe1f5fa64fe0f52d5ae0eff0ceeb26b
T.Mallika <tellakulamallika@gmailc.om>
2024-02-14 18:29:50 -0700

    firstcommit

---
+++ WebAppCustomLayout/WebAppCustomLayout/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppCustomLayout.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            ViewBag.msg = "List Of Customers";
+            return View();
+        }
+        public ActionResult NameList()
+        {
+            List<string> customers = new List<string>()
+            {
+            "Raj",
+            "Ravi",
+            "Nithin",
+            "Vijay",
+            "Dipesh"
+            };
+            ViewBag.NameList = customers;
+            return View();
+        }
+    }
+}
